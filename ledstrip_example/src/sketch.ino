b69618ecfc7369a06782972f8070a6ecaa2989c7
m0r13 <moritz.hilscher@gmail.com>
2016-09-03 02:16:57 +0200

    Some more tweaks.

--- ledstrip_example/src/sketch.ino
+++ ledstrip_example/src/sketch.ino
@@ -15,11 +15,60 @@ void setup() {
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
 }
- 
- 
+
+void setColor(int r, int g, int b) {
+  analogWrite(REDPIN, r);
+  analogWrite(GREENPIN, g);
+  analogWrite(BLUEPIN, b);
+};
+
+long HSV_to_RGB( float h, float s, float v ) {
+  h = h * 6.0;
+  /*
+     modified from Alvy Ray Smith's site:
+   http://www.alvyray.com/Papers/hsv2rgb.htm
+   H is given on [0, 6]. S and V are given on [0, 1].
+   RGB is returned as a 24-bit long #rrggbb
+   */
+  int i;
+  float m, n, f;
+
+  // not very elegant way of dealing with out of range: return black
+  if ((s<0.0) || (s>1.0) || (v<0.0) || (v>1.0)) {
+    return 0L;
+  }
+
+  if ((h < 0.0) || (h > 6.0)) {
+    return long( v * 255 ) + long( v * 255 ) * 256 + long( v * 255 ) * 65536;
+  }
+  i = floor(h);
+  f = h - i;
+  if ( !(i&1) ) {
+    f = 1 - f; // if i is even
+  }
+  m = v * (1 - s);
+  n = v * (1 - s * f);
+  switch (i) {
+  case 6:
+  case 0: // RETURN_RGB(v, n, m)
+    return long(v * 255 ) * 65536 + long( n * 255 ) * 256 + long( m * 255);
+  case 1: // RETURN_RGB(n, v, m) 
+    return long(n * 255 ) * 65536 + long( v * 255 ) * 256 + long( m * 255);
+  case 2:  // RETURN_RGB(m, v, n)
+    return long(m * 255 ) * 65536 + long( v * 255 ) * 256 + long( n * 255);
+  case 3:  // RETURN_RGB(m, n, v)
+    return long(m * 255 ) * 65536 + long( n * 255 ) * 256 + long( v * 255);
+  case 4:  // RETURN_RGB(n, m, v)
+    return long(n * 255 ) * 65536 + long( m * 255 ) * 256 + long( v * 255);
+  case 5:  // RETURN_RGB(v, m, n)
+    return long(v * 255 ) * 65536 + long( m * 255 ) * 256 + long( n * 255);
+  }
+}
+
 void loop() {
   int r, g, b;
  
+  /*
   // fade from blue to violet
   for (r = 0; r < 256; r++) { 
     analogWrite(REDPIN, r);
@@ -50,4 +99,33 @@ void loop() {
     analogWrite(GREENPIN, g);
     delay(FADESPEED);
   } 
+  */
+
+  /*
+  for (int i = 0; i < 3; i++) {
+    setColor(random(256), random(256), random(256));
+    delay(1000);
+  }
+
+  setColor(255, 255, 255);
+  delay(1000);
+  setColor(255, 0, 0);
+  delay(1000);
+  setColor(0, 255, 0);
+  delay(1000);
+  setColor(0, 0, 255);
+  delay(1000);
+  */
+
+  for (int i = 0; i < 256; i++) {
+    float h = float(i) / 255.0;
+    h = 1.0 / 6.0;
+    float s = 1.0;
+    float v = 0.2;
+
+    long rgb = HSV_to_RGB(h, s, v);
+    setColor((rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff);
+    delay(600);
+  }
+  
 }
